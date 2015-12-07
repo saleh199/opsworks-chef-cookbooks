bfbd8a8bbe464154f96f749802a3a31db26463f0
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-07 16:42:26 +0200

    Update update.rb

--- composer/recipes/update.rb
+++ composer/recipes/update.rb
@@ -9,7 +9,7 @@ node[:deploy].each do |application, deploy|
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
-    php composer.phar update
+    php composer.phar update --no-scripts
     EOH
     only_if { ::File.exists?("#{deploy[:deploy_to]}/current/composer.json") }
   end
