76f22b25e52045b5e0360633465e7d19196458b7
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-08 22:53:34 +0200

    Create script for run artisan commands

--- laravel/recipes/artisan.rb
+++ laravel/recipes/artisan.rb
@@ -1,1 +1,10 @@
-
+node[:deploy].each do |application, deploy|
+  script "artisan" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    php artisan #{node[:command]} #{node[:parameters]}
+    EOH
+  end
+end 
