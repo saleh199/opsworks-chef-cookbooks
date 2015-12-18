2f042309404777c15ebab96c1afb82c3fe215db9
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-18 11:45:26 +0200

    Update artisan.rb

--- laravel/recipes/artisan.rb
+++ laravel/recipes/artisan.rb
@@ -4,7 +4,9 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    php artisan #{node['command']} #{node['parameters']}
+    php artisan migrate --path=database/migrations/
+    php artisan migrate --path=database/migrations/2015_10_30
+    php artisan migrate --path=database/migrations/2015_11_01
     EOH
   end
 end 
