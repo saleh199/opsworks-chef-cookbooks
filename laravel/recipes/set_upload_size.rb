4aa64f990990a1847e6e24155fc48c79e3a6ec18
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-20 23:04:03 +0200

    Create set_upload_size.rb

---
+++ laravel/recipes/set_upload_size.rb
@@ -0,0 +1,12 @@
+ruby_block "php_ini" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/php5/apache2/php.ini")
+    file.insert_line_if_no_match("/upload_max_filesize = 2M/", "upload_max_filesize = 10M")
+    file.insert_line_if_no_match("/post_max_size = 8M/", "post_max_size = 10M")
+    file.write_file
+  end
+end
+
+service "apache2" do
+  action :restart
+end
