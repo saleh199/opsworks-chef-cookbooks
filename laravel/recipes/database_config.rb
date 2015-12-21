a936cb38b26a9e4b44c72a250d90ad425c680d47
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-21 18:37:08 +0200

    Update database_config.rb

--- laravel/recipes/database_config.rb
+++ laravel/recipes/database_config.rb
@@ -2,11 +2,11 @@ node[:deploy].each do |application, deploy|
   ruby_block "database_config" do
     block do
       file = Chef::Util::FileEdit.new("#{deploy[:deploy_to]}/current/config/database.php")
-      file.insert_line_if_no_match("/'default' => env('MYSQL_DB_CONNECTION'),/", "default' => 'mysql',")
-      file.insert_line_if_no_match("/'host' => env('MYSQL_DB_HOST'),/", "host' => '172.31.17.103',")
-      file.insert_line_if_no_match("/'database' => env('MYSQL_DB_DATABASE'),/", "database' => 'overestate',")
-      file.insert_line_if_no_match("/'username' => env('MYSQL_DB_USERNAME'),/", "username' => 'root',")
-      file.insert_line_if_no_match("/'password' => env('MYSQL_DB_PASSWORD'),/", "password' => '0verEst@te',")
+      file.search_file_replace("/'default' => env\('MYSQL_DB_CONNECTION'\),/", "default' => 'mysql',")
+      file.search_file_replace("/'host' => env\('MYSQL_DB_HOST'\),/", "host' => '172.31.17.103',")
+      file.search_file_replace("/'database' => env\('MYSQL_DB_DATABASE'\),/", "database' => 'overestate',")
+      file.search_file_replace("/'username' => env\('MYSQL_DB_USERNAME'\),/", "username' => 'root',")
+      file.search_file_replace("/'password' => env\('MYSQL_DB_PASSWORD'\),/", "password' => '0verEst@te',")
       file.write_file
     end
   end
