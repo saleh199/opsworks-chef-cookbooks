19c8a4e150e17cbda7a96bbc0447eafaf69c347f
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-23 18:21:57 +0200

    Update set_permission.rb

--- laravel/recipes/set_permission.rb
+++ laravel/recipes/set_permission.rb
@@ -1,4 +1,6 @@
-execute "chmod_laravel_public" do
-  command "chmod -R 777 /var/www/html/project-name/app/storage"
-  action :run
+node[:deploy].each do |application, deploy|
+  execute "chmod_laravel_public" do
+    command "chmod -R 777 #{deploy[:deploy_to]}/current/storage"
+    action :run
+  end
 end
