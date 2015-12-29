efc2f3f91708507f46d966339b8f8badb2b2b245
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-29 11:36:27 +0200

    Create database.rb

---
+++ laravel/attributes/database.rb
@@ -0,0 +1,5 @@
+default[:mysql][:driver]  = "mysql"
+default[:mysql][:host]  = "172.31.17.103"
+default[:mysql][:database]  = "overestate"
+default[:mysql][:username]  = "root"
+default[:mysql][:password]  = "0verEst@te"
