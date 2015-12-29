9b448de6f3e3b309124f967e0804e44c29e6963b
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-29 11:46:49 +0200

    Update database_config.rb

--- laravel/recipes/database_config.rb
+++ laravel/recipes/database_config.rb
@@ -1,13 +1,7 @@
 node[:deploy].each do |application, deploy|
-  ruby_block "database_config" do
-    block do
-      file = Chef::Util::FileEdit.new("#{deploy[:deploy_to]}/current/config/database.php")
-      file.search_file_replace("/'default' => env\('MYSQL_DB_CONNECTION'\),/", "'default' => 'mysql',")
-      file.search_file_replace("/'host' => env\('MYSQL_DB_HOST'\),/", "'host' => '172.31.17.103',")
-      file.search_file_replace("/'database' => env\('MYSQL_DB_DATABASE'\),/", "'database' => 'overestate',")
-      file.search_file_replace("/'username' => env\('MYSQL_DB_USERNAME'\),/", "'username' => 'root',")
-      file.search_file_replace("/'password' => env\('MYSQL_DB_PASSWORD'\),/", "'password' => '0verEst@te',")
-      file.write_file
-    end
+  template "#{deploy[:deploy_to]}/current/config/database.php" do
+    source "database.php.erb"
+    mode 0644
+    group deploy[:group]
   end
 end
