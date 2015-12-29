b93a3a39125ee72b5b76295c6e96d3ca5380f5f3
Saleh Saeed <saleh199@users.noreply.github.com>
2015-12-29 12:05:39 +0200

    Create filesystem.rb

---
+++ laravel/attributes/filesystem.rb
@@ -0,0 +1,4 @@
+default[:s3][:key]  = "AKIAIX7NPZKZZDMNABWA"
+default[:s3][:secret]  = "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"
+default[:s3][:region]  = "us-west-1"
+default[:s3][:bucket]  = "overestate"
