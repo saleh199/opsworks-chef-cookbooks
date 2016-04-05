8761a28c8ba7dca4b62f5cfc00da3958e7035180
Saleh Saeed <saleh199@users.noreply.github.com>
2016-04-05 16:07:25 +0300

    Update set_permission.rb

--- laravel/recipes/set_permission.rb
+++ laravel/recipes/set_permission.rb
@@ -2,14 +2,12 @@ node[:deploy].each do |application, deploy|
   execute "storage_permission" do
     command "chmod -R 777 #{deploy[:deploy_to]}/current/storage"
     action :run
-    only_if do
-     File.directory?("#{deploy[:deploy_to]}/current/storage")
+    only_if { ::File.directory?("#{deploy[:deploy_to]}/current/storage") }
   end
   
   execute "bootstrap_permission" do
     command "chmod -R 777 #{deploy[:deploy_to]}/current/bootstrap"
     action :run
-    only_if do
-     File.directory?("#{deploy[:deploy_to]}/current/bootstrap")
+    only_if { ::File.directory?("#{deploy[:deploy_to]}/current/bootstrap") }
   end
 end
