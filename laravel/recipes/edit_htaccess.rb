c3d096b00b77301ae2cf2abf7b102a461bda6e97
Saleh Saeed <saleh199@users.noreply.github.com>
2016-04-05 15:42:25 +0300

    Update edit_htaccess.rb

--- laravel/recipes/edit_htaccess.rb
+++ laravel/recipes/edit_htaccess.rb
@@ -3,5 +3,6 @@ node[:deploy].each do |application, deploy|
     source "htaccess.erb"
     mode 755
     group deploy[:group]
+    only_if { ::File.exists?("#{deploy[:deploy_to]}/current/public/.htaccess") }
   end
 end
