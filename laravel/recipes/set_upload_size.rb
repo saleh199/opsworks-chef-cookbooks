db92b7592ee0c6ead7be0af76c6207d24555d0de
Saleh Saeed <saleh199@users.noreply.github.com>
2016-04-05 15:53:45 +0300

    Update set_upload_size.rb

--- laravel/recipes/set_upload_size.rb
+++ laravel/recipes/set_upload_size.rb
@@ -4,9 +4,11 @@ ruby_block "php_ini" do
     file.insert_line_if_no_match("/upload_max_filesize = 2M/", "upload_max_filesize = 10M")
     file.insert_line_if_no_match("/post_max_size = 8M/", "post_max_size = 10M")
     file.write_file
+    only_if { ::File.exists?("/etc/php5/apache2/php.ini") }
   end
 end
 
 service "apache2" do
   action :restart
+  only_if { ::File.exists?("/etc/php5/apache2/php.ini") }
 end
